3f678892c94b5a742661e45abbbf25ef07b251b4
weezer562 <weezer562@gmail.com>
2024-07-17 01:48:57 -0700

    Conditional Rendering

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,11 +7,26 @@ import TabButton from './components/TabButton.jsx';
 import { EXAMPLES } from './data.js'; 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
     //console.log(`You clicked on ${selectedButton}`);
+  }
+
+  let tabContent = <p>Please select a Topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = 
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
   }
 
   return (
@@ -39,15 +54,7 @@ function App() {
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
