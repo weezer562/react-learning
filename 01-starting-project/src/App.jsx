3e991a8815ef6f3f434894dc35d6e2e13aacd0c1
weezer562 <weezer562@gmail.com>
2024-07-18 15:22:52 -0700

    Updated Fragment

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState, Fragment } from 'react'; 'react';
+import { useState } from 'react'; 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
@@ -30,7 +30,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -50,7 +50,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
