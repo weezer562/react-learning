0b0263873911d5b394d3cc48d7fdbfd97693398c
weezer562 <weezer562@gmail.com>
2024-07-22 16:01:13 -0700

    Updates TabButton.jsx to  forward  onClick prop

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({children, onClick, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onClick}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
         </li>
     );
 }
